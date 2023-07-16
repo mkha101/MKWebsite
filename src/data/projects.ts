02b6d7b20d12d19220c0a8ea249350a7d637ffe0
muzamilkhan <k.muzamil1001@gmail.com>
2023-07-16 19:30:51 -0400

    Skills logo added

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,15 +1,24 @@
 export type Project = {
   title: string;
   techs: string[];
+  description: string;
   link: string;
   isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
   {
+    title: "Kicks Avenue",
+    techs: ["ReactJS", "Stripe", "Redux", "MySQL", "Strapi"],
+    description: "E-Commerce website for purchasing sneakers.",
+    link: "kicksavenue.netlify.app",
+  },
+  {
     title: "Book Finder App",
-    techs: ["NextJS"],
-    link: "aibookfindeår.netlify.app",
+    techs: ["Next.js", "Open Ai API", "Google Books API"],
+    description:
+      "Web app made for searching books and generating summaries using Ai.",
+    link: "aibookfinder.netlify.app",
   },
 ];
 
